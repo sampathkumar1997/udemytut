74b112b7c1c7fc3138d65b4f0f0f1183dc09db36
Sampath Kumar <ssampathkumar1997@gmail.com>
2019-07-19 19:09:28 +0530

    First commit

---
+++ ng-refresher/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+@NgModule({
+  declarations: [
+    AppComponent, PersonsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ng-refresher/src/app/persons/persons.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector:'app-persons',
+  templateUrl: './persons.component.html'
+
+})
+
+export class PersonsComponent {
+ personList: string[];
+
+ }
